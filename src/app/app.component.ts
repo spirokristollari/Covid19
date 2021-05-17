0b9d5051831a3c5adb5e4b6cce967adcaa630c4d
Spiro Kristollari <skristo32@gmail.com>
2021-05-17 10:06:59 +0200

    covid website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoronaService } from './/corona.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'covid19';
+export class AppComponent implements OnInit{
+
+  countries: any;
+  country: any;
+  confirmed: Number;
+  recovered: Number;
+  deaths: Number;
+
+  constructor(private corona: CoronaService) {}
+
+  ngOnInit() {
+    this.corona.getCountries().subscribe((data) => {
+      this.countries = data;
+    })
+  }
+
+  getCoronaData() {
+    this.corona.getData(this.country).subscribe((data) => {
+      var index = data.length - 1;
+      this.confirmed = data[index].Confirmed;
+      this.recovered = data[index].Recovered;
+      this.deaths = data[index].Deaths;
+    })
+  }
+
+  getCountry(country: any) {
+    this.country = country;
+  }
 }
